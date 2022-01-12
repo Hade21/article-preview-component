2fbcd666b51614184e438d7073cd5b2e5757d3a4
Hade21 <abdurrohmanmuhammad123@gmail.com>
2022-01-12 20:45:18 +0700

    fix layouting

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ const popup = document.querySelector('.share-group')
 
 share.addEventListener('click', function() {
     popup.classList.toggle('show');
-    share.classList.toggle('active');
+    popup.classList.toggle('share-group-active');
+    popup.previousElementSibling.classList.toggle('share-active');
+    popup.parentElement.parentElement.classList.toggle('creator-active');
+    popup.parentElement.previousElementSibling.previousElementSibling.classList.toggle('avatar-active');
+    popup.parentElement.previousElementSibling.classList.toggle('name-active');
 });
